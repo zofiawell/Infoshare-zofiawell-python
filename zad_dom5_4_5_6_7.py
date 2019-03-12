45b0e97b84f1153d0c8ddbb4561cb8847037b1d6
zofiawell <zofia.wellenger@gmail.com>
2019-03-12 11:52:58 +0100

    zad dom 5 - poprawki

--- zad_dom5_4_5_6_7.py
+++ zad_dom5_4_5_6_7.py
@@ -25,7 +25,8 @@ stringi = ['abc', 'xyz', 'aba', '1221']
 stringi2 = []
 for string in stringi:
     if string[0] == string[-1]:
-        stringi2.append(string)
+        if len(string) > 2:
+            stringi2.append(string)
 
 odp = len(stringi2)
 print("są", odp, "takie stringi")
@@ -37,26 +38,27 @@ lista_a = [10,20,30,20,10,50,60,40,80,50,40]
 lista_b = []
 
 for wartosc in lista_a:
-    if wartosc not in lista_b:
+    if wartosc not in lista_b  and lista_a.count(wartosc) ==1:
         lista_b.append(wartosc)
-print(lista_b, "unikalne wartości")
+print(lista_b, "pojedyńcze wartości")
 
 
 
 # 7. program usuwajacy zduplikowane wartosci w liscie (w miejscu! - tzn bez drugiej listy)
-lista_c = [10,20,30,20,10,50,60,40,80,50,40]
-dl_listy_c = len(lista_c)
-lista_c = sorted(lista_c)
-print(lista_c)
-if lista_c[0] == lista_c[1]:
-    lista_c.remove(lista_c[1])
-if lista_c[1] == lista_c[2]:
-    lista_c.remove(lista_c[2])
-if lista_c[2] == lista_c[3]:
-    lista_c.remove(lista_c[3])
-if lista_c[3] == lista_c[4]:
-    lista_c.remove(lista_c[4])
-if lista_c[4] == lista_c[5]:
-    lista_c.remove(lista_c[4])
-print(lista_c)
+
+lista_d = [10,20,30,20,10,50,60,40,80,50,40]
+print(lista_d)
+wynik = list(set(lista_d))
+print(sorted(wynik))
+dlug = len(wynik)
+dlug = int(dlug)
+print(dlug)
+
+for liczba in wynik:
+    if 2 >= wynik.count(liczba):
+        wynik.remove(liczba)
+
+print(wynik)
+
+
 
